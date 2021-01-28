59cb700815d768f785f7be873d2fdf466e4b9d78
Dilan Jayamanne <dicmandilan@gmail.com>
2021-01-29 00:33:28 +0530

    updated capture screen

--- lib/screens/capture_screen.dart
+++ lib/screens/capture_screen.dart
@@ -4,6 +4,10 @@ class CaptureScreen extends StatelessWidget {
   static String id = "capture_screen";
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+
+      ),
+    );
   }
 }
