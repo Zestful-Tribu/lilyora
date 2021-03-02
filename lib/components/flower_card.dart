3783926926f2abb908b1636e192e861f2935ee38
dhanushka98 <58356502+dhanushka98@users.noreply.github.com>
2021-03-02 11:43:33 +0530

    Home screen second step added with sample images

---
+++ lib/components/flower_card.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+class FlowerCard extends StatelessWidget {
+  const FlowerCard({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 5, bottom: 5),
+      child: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(10),
+            height: 120,
+            decoration: BoxDecoration(
+              color: Colors.redAccent,
+              borderRadius: BorderRadius.circular(20),
+            ),
+            child: Row(
+              children: [
+                Container(
+                  height: 100,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    borderRadius: BorderRadius.circular(20),
+                    image: DecorationImage(
+                      image: AssetImage(
+                          "assets/flower.png"
+                      ),
+                    ),
+                  ),
+                ),
+
+                Expanded(
+                  child: Container(
+                    width: MediaQuery.of(context).size.height / 3,
+                    padding: EdgeInsets.only(left: 10, right: 10),
+                    child: Column(
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.height / 3,
+                          child: Text(
+                            "Rose Flower",
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          decoration: BoxDecoration(
+
+                            border: Border(
+                              bottom: BorderSide(
+                                color: Colors.black,
+                                width: 2,
+                              ),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 5, bottom: 5),
+                          child: Text(
+                              "This is a flower of srendib. flower of valentine"
+                                  "the heart of sea, and the name of great graat"
+                                  "flowers in the universe of flowers."
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:lilyora/components/scrolling_small_flowers_horizontal.dart';
+import 'package:lilyora/screens/search_screen.dart';
+
+import 'capture_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   static String id = "home_screen";
@@ -19,13 +23,168 @@ class _HomePageState extends State<HomePage>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: 300,
-        height: 200 ,
-        color: Colors.grey,
-        child: Padding(padding: EdgeInsets.only(top: 100.0),),
-        margin: EdgeInsets.all(15.0),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.purpleAccent,
+        onPressed: () {
+          Navigator.pushNamed(context, CaptureScreen.id);
+        },
+        tooltip: 'capture',
+        child: Icon(
+          Icons.camera,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
 
+
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Container(
+
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Text(
+                "Popular flowers",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              height: 70.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Text(
+                "Flower of the day",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Colors.yellow,
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Container(
+                        height: 100,
+                        width: 100,
+                        decoration: BoxDecoration(
+                          color: Colors.tealAccent,
+                          borderRadius: BorderRadius.circular(20),
+
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          padding: EdgeInsets.all(5),
+                          height: 100,
+                          width: MediaQuery.of(context).size.height / 3,
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(4),
+                                width: MediaQuery.of(context).size.height / 3,
+
+                                decoration: BoxDecoration(
+
+                                  border: Border(
+                                    bottom: BorderSide(
+                                      color: Colors.black,
+                                      width: 2,
+                                    ),
+                                  ),
+                                ),
+                                child: Text(
+                                    "Rose Flower"
+                                ),
+                              ),
+                              Container(
+
+                                child: Text(
+                                    "Genus: Rosa L\n" +
+                                        "Family: Rosaceae\n"+
+                                        "Kindgdom: Platae"
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                        "A rose is awoody perennial asdfa aiosdfhosdhf dahsdfoauh sdfoahda dfoh"
+                            "asdfha isdufhao dfadfojaosdfhao dadfaidfh "
+                            "adfhaodfiaoid asdf aisdfha;o as"
+                            "aa oisdfhaosh a;s dfhsodhfo sdfpa sdfp"
+                            "aoisdf aidf"
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            Container(
+              padding: EdgeInsets.only(top: 10, bottom: 10),
+              child: Text(
+                "Recently searched",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              height: 70.0,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                  smallFlowerImg(),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
 
     );
