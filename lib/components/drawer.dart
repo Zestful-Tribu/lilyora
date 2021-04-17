dc8d8a42895048e6ce32353a69a6b8f412e8f2f4
dhanushka98 <58356502+dhanushka98@users.noreply.github.com>
2021-04-18 00:37:49 +0530

    Adding profile image

--- lib/components/drawer.dart
+++ lib/components/drawer.dart
@@ -8,17 +8,36 @@ class LilyoraDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
+      child: Column(
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            color: Theme.of(context).primaryColor,
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Container(
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      image: DecorationImage(
+                          image: AssetImage(
+                              "assets/name.jpg"
+                          ),
+                          fit: BoxFit.fill
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
 
-        child: Column(
-          children: <Widget>[
-            Container(
-              width: double.infinity,
-            )
-          ],
-        ),
-
-      );
-
+    );
     }
       /**
        * git add .
